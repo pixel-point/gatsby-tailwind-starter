70f3b0cc4b2b712fd1151bc3655efe7bd12032f6
Alex Barashkov <lnikell@gmail.com>
2021-02-17 14:42:15 +0100

    fix: wrong loader configuration

--- gatsby-config.js
+++ gatsby-config.js
@@ -74,10 +74,8 @@ module.exports = {
       resolve: 'gatsby-plugin-sass',
       options: {
         additionalData: '@import "./src/styles/variables.scss" , "./src/styles/mixins.scss";',
-        sassOptions: {
-          cssLoaderOptions: {
-            camelCase: true,
-          },
+        cssLoaderOptions: {
+          camelCase: true,
         },
       },
     },
--- src/components/shared/header/header.jsx
+++ src/components/shared/header/header.jsx
@@ -4,9 +4,9 @@ import React from 'react';
 import styles from './header.module.scss';
 
 const cx = classNames.bind(styles);
-
+console.log(styles);
 const Header = () => (
-  <header className={cx('wrapper', { wrapperTest: true })}>
+  <header className={cx('wrapper', 'wrapper-test')}>
     <div className="container">Header</div>
   </header>
 );
