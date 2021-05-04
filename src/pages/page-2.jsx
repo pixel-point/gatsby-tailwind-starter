e80e29b14ea6bf0f2ae010a86b69a81ba355f090
Alex Barashkov <lnikell@gmail.com>
2021-05-04 14:20:46 +0200

    fix: update gatsby-plugin-svgr-svgo

--- src/pages/page-2.jsx
+++ src/pages/page-2.jsx
@@ -1,15 +1,11 @@
 import React from 'react';
 
 import Link from 'components/shared/link';
-import ExampleInlineSVG from 'images/example.inline.svg';
-import exampleUrlSvg from 'images/example.svg';
 import MainLayout from 'layouts/main';
 
 const IndexPage = () => (
   <MainLayout>
     <div className="container">
-      Inline SVG: <ExampleInlineSVG />
-      URL SVG: <img src={exampleUrlSvg} alt="" />
       <Link to="/">Back</Link>
     </div>
   </MainLayout>
