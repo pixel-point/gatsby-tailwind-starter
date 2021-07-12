b0118e986540860a43ce02176e5e50dad89f7d8e
vannnyle@gmail.com <vannnyle@gmail.com>
2021-07-12 10:08:29 +0300

    chore: enable mode JIT

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
+  mode: 'jit',
   purge: ['./src/**/*.{js,jsx,ts,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
