e9a5243bd773d10af4cc4bdcba31f6cacfe857cb
Maksim Gorodov <silencer.web@gmail.com>
2021-11-22 16:18:37 +0500

    refactor: fix typo

--- src/components/shared/seo/seo.jsx
+++ src/components/shared/seo/seo.jsx
@@ -3,7 +3,7 @@ import { useStaticQuery, graphql } from 'gatsby';
 import React from 'react';
 import { Helmet } from 'react-helmet';
 
-const SEO = ({ title, descripton }) => {
+const SEO = ({ title, description }) => {
   const {
     site: {
       siteMetadata: { siteTitle, siteDescription, siteUrl, siteImage, siteLanguage },
@@ -31,10 +31,10 @@ const SEO = ({ title, descripton }) => {
       }}
     >
       {/* General */}
-      <meta name="description" content={siteDescription || descripton} />
+      <meta name="description" content={siteDescription || description} />
       {/* Open Graph */}
       <meta property="og:title" content={siteTitle || title} />
-      <meta property="og:description" content={siteDescription || descripton} />
+      <meta property="og:description" content={siteDescription || description} />
       <meta property="og:url" content={siteUrl} />
       <meta property="og:image" content={siteUrl + siteImage} />
       <meta property="og:type" content="website" />
