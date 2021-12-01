0fb40c9f494c6d51928438185b32527e8081c3db
Maksim Gorodov <silencer.web@gmail.com>
2021-12-01 16:48:52 +0500

    feat: add components

--- src/components/shared/footer/footer.jsx
+++ src/components/shared/footer/footer.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Footer = () => (
-  <footer>
+  <footer className="sticky top-[100vh] safe-paddings">
     <div className="container">Footer!</div>
   </footer>
 );
