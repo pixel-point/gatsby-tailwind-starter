88aa5c756203e9625da0419db1644637bd6129bf
Maksim Gorodov <silencer.web@gmail.com>
2021-12-03 16:58:34 +0500

    fix: add some fixes

--- gatsby-config.js
+++ gatsby-config.js
@@ -57,7 +57,6 @@ module.exports = {
                   },
                 },
                 'prefixIds',
-                // 'removeDimensions',
               ],
             },
           },
@@ -72,6 +71,16 @@ module.exports = {
         ],
       },
     },
+    // TODO: Either uncomment this part of the code if the website is being hosted on Netlify or delete it
+    // {
+    //   resolve: 'gatsby-plugin-netlify',
+    //   // TODO: Either uncomment this part of the code if fonts are stored in /static/fonts/ or delete it
+    //   // options: {
+    //   //   headers: {
+    //   //     '/fonts/*': ['Cache-Control: public, max-age: 31536000, immutable'],
+    //   //   },
+    //   // },
+    // },
     'gatsby-alias-imports',
     'gatsby-plugin-postcss',
     'gatsby-plugin-sitemap',
